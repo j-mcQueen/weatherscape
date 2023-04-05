48b55162e67cf811bb6f7dd6e769831c37175c9b
jack mcqueen <jdmcqueen44@gmail.com>
2023-04-05 16:35:19 -0400

    Import header components and apply desktop styles

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "./header.css";
+import Logo from "./Logo";
+import Search from "./Search";
+
+export default function Header() {
+    return (
+        <header>
+            <Logo/>
+            <Search/>
+        </header>
+    )
+}
