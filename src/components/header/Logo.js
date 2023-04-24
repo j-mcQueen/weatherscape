d2f399bd762de20993d3584732796826d41bf626
jack mcqueen <jdmcqueen44@gmail.com>
2023-04-24 13:11:27 -0400

    Assorted minor fixes

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -2,7 +2,7 @@ export default function Logo() {
     return (
         <div className="logo-container">   
             <span className="logo-box"></span>
-            <span className="logo-name">WEATHERSCOPE</span>
+            <span className="logo-name">weatherscope</span>
         </div>
     )
 }
