56632bd21416112c041089c6d0823b44e73496b0
jack mcqueen <jdmcqueen44@gmail.com>
2023-04-28 14:24:43 -0400

    Add cycle component for forecast navigation

---
+++ src/components/main/Cycle.js
@@ -0,0 +1,27 @@
+export default function Cycle({...props}) {
+    return (
+        <div className="cycle">
+            <div className="cycle-container">
+                <div className="cycle-btns">
+                    <button 
+                        type="button"
+                        onClick={() => props.setCycle(1)}
+                        className={props.cycle === 1 ? "active" : null}>
+                    </button>
+
+                    <button 
+                        type="button"
+                        onClick={() => props.setCycle(2)}
+                        className={props.cycle === 2 ? "active" : null}>
+                    </button>
+
+                    <button 
+                        type="button"
+                        onClick={() => props.setCycle(3)}
+                        className={props.cycle === 3 ? "active" : null}>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+} 
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,7 @@
-import Details from "./Details";
 import "./main.css";
 import Overview from "./Overview";
+import Cycle from "./Cycle";
+import Details from "./Details";
 
 export default function Main({...props}) {
     return (
@@ -8,6 +9,10 @@ export default function Main({...props}) {
             <Overview
                 weather={props.weather}
                 temp={props.temp}
+            />
+            <Cycle
+                cycle={props.cycle}
+                setCycle={props.setCycle}
             />
             <Details
                 weather={props.weather}
