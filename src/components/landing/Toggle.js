958b8eda4b9f6d868fc839011d1a54d22aee191e
jack mcqueen <jdmcqueen44@gmail.com>
2023-07-20 12:26:53 -0400

    Add values to unit buttons for a11y

--- src/components/landing/Toggle.js
+++ src/components/landing/Toggle.js
@@ -1,20 +1,24 @@
-export default function Toggle({...props}) {
-    return (
-        <div className="toggle">
-            <div className="toggle-container">
-                <span>METRIC</span>
-                <div className="toggle-btns">
-                    <button
-                        onClick={() => props.setUnit("metric")}
-                        className={ props.unit === "metric" ? "active" : null }
-                        type="button"></button>
-                    <button
-                        onClick={() => props.setUnit("imperial")}
-                        className={ props.unit === "imperial" ? "active" : null }
-                        type="button"></button>
-                </div>
-                <span>IMPERIAL</span>
-            </div>
+export default function Toggle({ ...props }) {
+  return (
+    <div className="toggle">
+      <div className="toggle-container">
+        <span>METRIC</span>
+        <div className="toggle-btns">
+          <button
+            value={"Metric"}
+            onClick={() => props.setUnit("metric")}
+            className={props.unit === "metric" ? "active" : null}
+            type="button"
+          ></button>
+          <button
+            value={"Imperial"}
+            onClick={() => props.setUnit("imperial")}
+            className={props.unit === "imperial" ? "active" : null}
+            type="button"
+          ></button>
         </div>
-    )
+        <span>IMPERIAL</span>
+      </div>
+    </div>
+  );
 }
